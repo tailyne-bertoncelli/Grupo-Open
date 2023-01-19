9928a44c17d78c26940272193d181514785a8d3a
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-01-19 14:20:08 -0300

    Primeiro Commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let botaoSobre = document.querySelector(".botaoSobre");
+botaoSobre.addEventListener("click", aviso);
+
+function aviso(){
+    alert("Em desenvolvimento!");
+}
+
+
+function exibeAlerta() {
+    alert("Documento privado!");
+}
